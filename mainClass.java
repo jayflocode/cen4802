7022cb0c8428a024eb50740c5c6e2a4516a2c28e
Jayson <112125717+jayflocode@users.noreply.github.com>
2024-08-24 15:35:41 -0400

    Main Commit

---
+++ mainClass.java
@@ -0,0 +1,59 @@
+
+package org.example;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Fibonacci Application takes a fibonacci term and return the value of that term.
+ * The Application uses a recursive method to calculate the information.
+ * @author Jayson Flores
+ * @version 1.0
+ */
+
+public class mainClass {
+    
+
+    static int recursive(int n) {
+      //returns 0 if parameter equals to 0, or returns 1
+      if (n == 0) {
+          return 0;
+      }
+
+      if (n == 1) {
+
+          return 1;
+      }
+
+      // executes recursive method call given that the number is neither 0 nor 1
+
+      else {
+          // Fn =  Fn - 1 + Fn - 2
+          return recursive(n - 1) + recursive(n - 2);
+      }
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+                int input;
+
+                System.out.println("Please Enter Integer");
+
+                input = scan.nextInt();
+
+
+                // print command includes term and calls recursive method
+
+                System.out.println("The " + input + "th term is " + recursive(input));
+
+
+
+
+    }
+}
